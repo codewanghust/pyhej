2823aa693178a26a975a4aa4f1a0a4e1507452d0
flystarhe <flystarhe@qq.com>
2017-08-17 10:32:56 +0800

    2017-08-17

---
+++ genplate/readme.md
@@ -0,0 +1,21 @@
+车牌生成器,参考[szad670401](https://github.com/szad670401/end-to-end-for-chinese-plate-recognition)实现,对代码进行了精简,支持py3.依赖项:
+
+```
+pip install pillow opencv numpy
+```
+
+建议编译安装OpenCV3.2,包含扩展部分.
+
+运行示例:
+```
+import sys
+sys.path.append('/data2/gits')
+from pyhej.genplate import genplate
+G = genplate.GenPlate()
+G.gen_batch(10, './temp', (272,72))
+
+from pyhej.image_view import get_plt_show
+get_plt_show(['./temp/%03d.jpg' % i for i in range(10)], height=0.3).show();
+```
+
+![](readme_01.png)
--- image_view.py
+++ image_view.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
-from pyhej.image import load_img, img_to_array
-def get_plt_show(fpaths, col=5, row=None, target_size=None):
-    if row is None:
-        row = int(len(fpaths)/col) + 1
-    plt.figure(figsize=(18, int(row/col*18)))
+from pyhej.image import load_img
+def get_plt_show(fpaths, col=5, height=1.0, target_size=None):
+    row = int(len(fpaths)/col) + 1
+    plt.figure(figsize=(18, int(row/col*18*height)))
     for i in range(row):
         for j in range(col):
             num = i*col + j
             if len(fpaths) > num:
                 ax = plt.subplot(row, col, num + 1)
                 img = load_img(fpaths[num], target_size=target_size)
-                arr = img_to_array(img)
-                plt.imshow(arr)
+                plt.imshow(img)
                 ax.get_xaxis().set_visible(False)
                 ax.get_yaxis().set_visible(False)
             else:
-                break
+                return plt
     return plt
 
