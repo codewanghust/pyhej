794d034a57a1d4ee3183f7a90d53e275880f9a58
flystarhe <flystarhe@qq.com>
2017-09-04 13:33:51 +0800

    2017 0904

--- nlp/string.py
+++ nlp/string.py
@@ -1,24 +1,4 @@
 import re
-def name_std(text):
-    """
-    Name standard
-
-    # Arguments
-        text: a string
-
-    # Returns
-        A string
-
-    # Raises
-        ..
-    """
-    tmp = text.lower()
-    tmp = re.sub(r'[()]', '', tmp)
-    tmp = re.sub(r'[\s,]+', ' ', tmp)
-    return tmp
-
-
-import re
 def normalize(text):
     tmp = text.lower()
     tmp = re.sub(r'[\s,]+', ' ', tmp)
