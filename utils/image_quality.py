a185bf7f54ae3ed09f118eb1d350958785adeb1d
flystarhe <flystarhe@qq.com>
2017-11-16 14:04:47 +0800

    17 11 16

---
+++ utils/image_quality.py
@@ -0,0 +1,66 @@
+'''
+=> http://blog.csdn.net/real_myth/article/details/50827940
+无参考图像的清晰度评价方法
+'''
+import cv2
+
+
+def eval_of_laplacian(image_path):
+    '''http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+    http://python.jobbole.com/83702/
+    这种方法凑效的原因就在于拉普拉斯算子定义本身:
+      它被用来测量图片的二阶导数,突出图片中强度快速变化的区域
+      和Sobel以及Scharr算子十分相似,也经常用于边缘检测
+    此算法基于以下假设:
+      如果图片具有较高方差,那么它就有较广的频响范围,代表着正常,聚焦准确的图片
+      如果图片具有较小方差,那么它就有较窄的频响范围,意味着图片中的边缘数量很少
+      正如我们所知道的,图片越模糊,其边缘就越少
+    '''
+    img = cv2.imread(image_path)
+    if img.ndim == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = cv2.Laplacian(img, cv2.CV_64F).var()
+    return res
+
+
+def eval_of_reblur(image_path, ksize=5):
+    '''
+    如果图像模糊了,对它进行一次模糊处理,高频分量变化不大
+    如果原图是清晰的,对它进行一次模糊处理,高频分量变化会非常大
+      待评测图像 => 高斯模糊处理 => 退化的图像 => 比较原图像和退化图像的变化情况
+      - cv2.blur
+      - cv2.GaussianBlur
+      - cv2.medianBlur
+    '''
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img_blur = cv2.medianBlur(img, ksize)
+    res = cv2.Laplacian(img, cv2.CV_64F).var()
+    res_blur = cv2.Laplacian(img_blur, cv2.CV_64F).var()
+    return (res-res_blur)/res
+
+
+def eval_of_sobel(image_path, ksize=5):
+    '''http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+    '''
+    img = cv2.imread(image_path, 0)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=ksize)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=ksize)
+    res = (sobelx + sobely).var()
+    #from matplotlib import pyplot as plt
+    #plt.imshow(sobelx + sobely, cmap='gray')
+    #plt.show()
+    return res/4
+
+
+'''
+import cv2
+from matplotlib import pyplot as plt
+from pyhej.utils.image_quality import eval_of_laplacian
+
+plt.figure(figsize=(16, 16))
+for i in range(4):
+    image_path = '/data2/datasets/blurred-pictures/{}.jpg'.format(i+1)
+    plt.subplot(2,2,i+1), plt.imshow(cv2.imread(image_path, 0), cmap='gray')
+    plt.title(eval_of_laplacian(image_path)), plt.xticks([]), plt.yticks([])
+plt.show()
+'''
