d74bcb5d83dbb7b241021b0d535808af2d144ddc
flystarhe <flystarhe@qq.com>
2017-11-29 11:22:09 +0800

    17 11 29

---
+++ pytorch/classifier/DenseNet.py
@@ -0,0 +1,12 @@
+'''
+>>> base
+https://github.com/liuzhuang13/DenseNet
+
+>>> keras
+https://github.com/tdeboissiere/DeepLearningImplementations/tree/master/DenseNet
+
+>>> pytorch
+https://github.com/gpleiss/efficient_densenet_pytorch
+https://github.com/bamos/densenet.pytorch
+https://github.com/andreasveit/densenet-pytorch
+'''
