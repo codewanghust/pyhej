58a010d25767b5a6b6f95f0ef235b6efc8a90d9c
flystarhe <flystarhe@qq.com>
2017-12-11 22:37:54 +0800

    17 12 11

---
+++ utils/markdown.py
@@ -0,0 +1,29 @@
+import os
+import codecs
+
+
+class MD(object):
+    def __init__(self, path, name):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        if not name.endswith('.md'):
+            name += '.md'
+
+        fpath = os.path.join(path, name)
+        self.writer = codecs.open(fpath, 'w', 'utf-8')
+
+    def add_text(self, text):
+        self.writer.write(text)
+        self.writer.write('\n')
+
+    def add_topic(self, topic, level=2):
+        self.writer.write('#'*level + ' ' + topic)
+        self.writer.write('\n')
+
+    def add_image(self, fpath):
+        self.writer.write('![]({})'.format(fpath))
+        self.writer.write('\n')
+
+    def close(self):
+        self.writer.close()
