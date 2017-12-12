319a4be15df9a34550669ff97ce7647bed9cf8ae
flystarhe <flystarhe@qq.com>
2017-12-12 12:30:02 +0800

    17 12 12

--- sr/__init__.py
+++ sr/__init__.py
@@ -54,19 +54,23 @@ def imprint(img_h, img_b, img_gt=None, text=None, filename=None):
     img_b: A file path
     img_gt: A file path
     '''
-    wid, hei = img_h.size
     img_b = Image.open(img_b).convert('RGB')
+
+    wid_b, hei_b = img_b.size
+    wid_h, hei_h = img_h.size
 
     if img_gt:
         img_gt = Image.open(img_gt).convert('RGB')
-        out = image_new((wid*3, hei), (255, 255, 255))
+        wid_gt, hei_gt = img_gt.size
+
+        out = image_new((wid_gt+wid_b+wid_h, max(hei_gt, hei_h)), (0, 0, 0))
         out.paste(img_gt, (0, 0))
-        out.paste(img_b , (0+wid, 0))
-        out.paste(img_h , (0+wid*2, 0))
+        out.paste(img_b , (0+wid_gt, (hei_h-hei_b)//2))
+        out.paste(img_h , (0+wid_gt+wid_b, 0))
     else:
-        out = image_new((wid*2, hei), (255, 255, 255))
-        out.paste(img_b, (0, 0))
-        out.paste(img_h, (0+wid, 0))
+        out = image_new((wid_b+wid_h, hei_h), (0, 0, 0))
+        out.paste(img_b, (0, (hei_h-hei_b)//2))
+        out.paste(img_h, (0+wid_b, 0))
 
     if text:
         draw_text(out, (0, 0), text)
