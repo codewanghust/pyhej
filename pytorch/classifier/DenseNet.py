40c6f643706bee6497234f2e46815a462ba729e0
flystarhe <flystarhe@qq.com>
2017-12-28 17:16:07 +0800

    17 12 28

--- pytorch/classifier/DenseNet.py
+++ pytorch/classifier/DenseNet.py
@@ -6,6 +6,8 @@ https://github.com/liuzhuang13/DenseNet
 https://github.com/tdeboissiere/DeepLearningImplementations/tree/master/DenseNet
 
 >>> pytorch
+https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
+https://github.com/pytorch/examples/blob/master/imagenet/main.py
 https://github.com/gpleiss/efficient_densenet_pytorch
 https://github.com/bamos/densenet.pytorch
 https://github.com/andreasveit/densenet-pytorch
